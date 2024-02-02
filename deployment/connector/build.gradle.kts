d8ad9309e37388e779a71df9476f027a87c9bca0
fuqiang <qiang3.fu@changhong.com>
2024-02-02 10:01:33 +0800

    use mirrors maven

--- deployment/connector/build.gradle.kts
+++ deployment/connector/build.gradle.kts
@@ -5,7 +5,12 @@ plugins {
 }
 
 repositories {
-    mavenCentral()
+  maven { url = uri("https://maven.aliyun.com/repository/public/") }
+  maven { url = uri("https://maven.aliyun.com/repositories/jcenter") }
+  maven { url = uri("https://maven.aliyun.com/repositories/google") }
+  maven { url = uri("https://maven.aliyun.com/repositories/central") }
+  google()
+  mavenCentral()
 }
 
 dependencies {
