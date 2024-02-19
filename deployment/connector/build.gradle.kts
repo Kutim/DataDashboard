7b26eeec6b3276563d78c5f76b5b6aef33c6ef3a
fuqiang <qiang3.fu@changhong.com>
2024-02-19 13:56:07 +0800

    修改为使用 toml 配置

--- deployment/connector/build.gradle.kts
+++ deployment/connector/build.gradle.kts
@@ -25,59 +25,58 @@ dependencies {
 
     implementation(libs.edc.api.core)
     implementation(libs.edc.api.observability)
+    implementation(libs.edc.asset.index.sql)
     implementation(libs.edc.auth.spi)
     implementation(libs.edc.auth.tokenbased)
     implementation(libs.edc.boot)
 //    implementation(libs.edc.build.plugin)
     implementation(libs.edc.configuration.filesystem)
     implementation(libs.edc.connector.core)
+    implementation(libs.edc.contract.definition.store.sql)
+    implementation(libs.edc.contract.negotiation.store.sql)
     implementation(libs.edc.control.plane.api.client)
     implementation(libs.edc.control.plane.api)
     implementation(libs.edc.control.plane.core)
     implementation(libs.edc.control.plane.spi)
     implementation(libs.edc.control.plane.sql)
-
-  implementation(libs.edc.data.plane.api)
+    implementation(libs.edc.core.spi)
+    implementation(libs.edc.data.plane.api)
 //    implementation(libs.edc.data.plane.aws.s3)
 //    implementation(libs.edc.data.plane.azure.storage)
     implementation(libs.edc.data.plane.client)
     implementation(libs.edc.data.plane.core)
     implementation(libs.edc.data.plane.http)
+    implementation(libs.edc.data.plane.instance.store.sql)
     implementation(libs.edc.data.plane.kafka)
     implementation(libs.edc.data.plane.selector.api)
     implementation(libs.edc.data.plane.selector.client)
     implementation(libs.edc.data.plane.selector.core)
     implementation(libs.edc.data.plane.spi)
+    implementation(libs.edc.data.plane.store.sql)
     implementation(libs.edc.data.plane.util)
     implementation(libs.edc.dsp)
+    implementation(libs.edc.federated.catalog.core)
+    implementation(libs.edc.federated.catalog.api)
     implementation(libs.edc.http)
     implementation(libs.edc.iam.mock)
+//    implementation("org.eclipse.edc:identity-hub-credentials-store-sql:0.4.1")
     implementation(libs.edc.jersey.micrometer)
     implementation(libs.edc.jetty.micrometer)
     implementation(libs.edc.json.ld)
     implementation(libs.edc.management.api)
     implementation(libs.edc.micrometer.core)
     implementation(libs.edc.monitor.jdk.logger)
+    implementation(libs.edc.participant.store.sql)
+    implementation(libs.edc.policy.definition.store.sql)
+    implementation(libs.edc.policy.monitor.store.sql)
 //    implementation(libs.edc.provision.aws.s3)
     implementation(libs.edc.runtime.metamodel)
-
-    implementation("org.eclipse.edc:asset-index-sql:0.4.1")
-    implementation("org.eclipse.edc:contract-definition-store-sql:0.4.1")
-    implementation("org.eclipse.edc:contract-negotiation-store-sql:0.4.1")
-    implementation("org.eclipse.edc:control-plane-sql:0.4.1")
-    implementation("org.eclipse.edc:data-plane-instance-store-sql:0.4.1")
-    implementation("org.eclipse.edc:data-plane-store-sql:0.4.1")
-//    implementation("org.eclipse.edc:identity-hub-credentials-store-sql:0.4.1")
-    implementation("org.eclipse.edc:participant-store-sql:0.4.1")
-    implementation("org.eclipse.edc:policy-definition-store-sql:0.4.1")
-    implementation("org.eclipse.edc:policy-monitor-store-sql:0.4.1")
-    implementation("org.eclipse.edc:sql-core:0.4.1")
-    implementation("org.eclipse.edc:sql-lease:0.4.1")
-    implementation("org.eclipse.edc:sql-pool-apache-commons:0.4.1")
-    implementation("org.eclipse.edc:transaction-local:0.4.1")
-    implementation("org.eclipse.edc:transfer-process-store-sql:0.4.1")
-    implementation("org.eclipse.edc:transaction-datasource-spi:0.4.1")
-
+    implementation(libs.edc.sql.core)
+    implementation(libs.edc.sql.lease)
+    implementation(libs.edc.sql.pool.apache.commons)
+    implementation(libs.edc.transaction.datasource.spi)
+    implementation(libs.edc.transaction.local)
+    implementation(libs.edc.transfer.process.store.sql)
     implementation(libs.edc.transfer.data.plane)
     implementation(libs.edc.transfer.process.api)
     implementation(libs.edc.transfer.pull.http.receiver)
@@ -86,11 +85,6 @@ dependencies {
     implementation(libs.edc.validator.core)
 //    implementation(libs.edc.vault.azure)
     implementation(libs.edc.vault.filesystem)
-
-
-    implementation(libs.edc.core.spi)
-    implementation(libs.edc.federated.catalog.core)
-    implementation(libs.edc.federated.catalog.api)
 
     implementation(libs.jakarta.rsApi)
     implementation(libs.jakartaJson)
@@ -101,44 +95,6 @@ dependencies {
 //    implementation(libs.opentelemetry.annotations)
 
     implementation(libs.postgres)
-
-
-
-
-
-
-
-
-
-//  implementation(libs.edc.auth.spi)
-//    implementation(libs.edc.auth.tokenbased)
-//    implementation(libs.edc.control.plane.api.client)
-//    implementation(libs.edc.control.plane.api)
-//    implementation(libs.edc.control.plane.core)
-//    implementation(libs.edc.core.spi)
-//    implementation(libs.edc.dsp)
-//    implementation(libs.edc.configuration.filesystem)
-//    implementation(libs.edc.vault.filesystem)
-//    implementation(libs.edc.iam.mock)
-//    implementation(libs.edc.management.api)
-//    implementation(libs.edc.api.observability)
-//    implementation(libs.edc.transfer.data.plane)
-//    implementation(libs.edc.transfer.pull.http.receiver)
-//
-//    implementation(libs.edc.data.plane.selector.api)
-//    implementation(libs.edc.data.plane.selector.core)
-//    implementation(libs.edc.data.plane.selector.client)
-//
-//    implementation(libs.edc.data.plane.api)
-//    implementation(libs.edc.data.plane.core)
-//    implementation(libs.edc.data.plane.http)
-//
-//    implementation(libs.edc.federated.catalog.core)
-//    implementation(libs.edc.federated.catalog.api)
-//
-//    implementation(libs.edc.runtime.metamodel)
-
-
 
 }
 
