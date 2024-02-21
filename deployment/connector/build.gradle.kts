1278c32a997932e9b0e0faffa19b1d13633ca3cc
fuqiang <qiang3.fu@changhong.com>
2024-02-21 09:52:09 +0800

    java.util.logging 日志配置 JDK Logger Monitor

--- deployment/connector/build.gradle.kts
+++ deployment/connector/build.gradle.kts
@@ -17,9 +17,9 @@ repositories {
 
 dependencies {
     // SLF4J API
-    implementation("org.slf4j:slf4j-api:1.7.36") // 使用你需要的版本
+//    implementation("org.slf4j:slf4j-api:1.7.36") // 使用你需要的版本
     // Logback Classic Implementation
-    implementation("ch.qos.logback:logback-classic:1.4.12") // 使用你需要的版本
+//    implementation("ch.qos.logback:logback-classic:1.4.12") // 使用你需要的版本
 
 
 
